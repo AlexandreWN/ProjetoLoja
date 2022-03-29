fef8dc243fdc5eb8587da75b717fe4830849e4af
Alexandre <alexandrenikitin@outlook.com>
2022-03-29 14:54:19 -0300

    gets e sets de Stocks

--- Aulas_C/Model/Stocks.cs
+++ Aulas_C/Model/Stocks.cs
@@ -4,4 +4,10 @@ public class Stocks
     private Store store; // dependencia a Store
     private Product product;// dependencia de Product com Stocks
     private int quantity;
+
+    //GETS
+    public int getQuantity() { return quantity; }
+
+    //SETS
+    public void setQuantity(int quantity) { this.quantity = quantity; }
 }
