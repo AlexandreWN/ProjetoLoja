89c081c6efd3b921ecd2a627446c5e630acd34e0
Thiago <thiagopedron70@gmail.com>
2022-03-29 14:43:05 -0300

    Corrigindo dependencias

--- Aulas_C/Model/Product.cs
+++ Aulas_C/Model/Product.cs
@@ -1,7 +1,6 @@
 namespace Model;
 public class Product
-{
-    private Stocks stocks// dependencia de Product com Stocks
+{ 
     private string name;
     private decimal unit_price;
     private string bar_code;
--- Aulas_C/Model/Stocks.cs
+++ Aulas_C/Model/Stocks.cs
@@ -1,5 +1,7 @@
 namespace Model;
 public class Stocks
 {
+    private Store store; // dependencia a Store
+    private Product product;// dependencia de Product com Stocks
     private int quantity;
 }
