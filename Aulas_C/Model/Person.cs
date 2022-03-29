d07094768382d8f57f2e42fe0497a4d57449bb62
Alexandre <alexandrenikitin@outlook.com>
2022-03-29 15:39:02 -0300

    get e set Person

--- Aulas_C/Model/Person.cs
+++ Aulas_C/Model/Person.cs
@@ -9,4 +9,67 @@ public class Person
     protected string email;
     protected string phone;
     protected string login;
+
+    protected Person()
+    {
+
+    }
+    protected Person(Addres addres)
+    {
+        this.address = addres.address;
+    }
+
+    public string getName()
+    {
+        return name;
+    }
+    public void setName(string name)
+    {
+        this.name = name;
+    }
+
+    public int getAge()
+    {
+        return age;
+    }
+    public void setAge(int age)
+    {
+        this.age = age;
+    }
+
+    public string getDocument()
+    {
+        return document;
+    }
+    public void setDocument(string document)
+    {
+        this.document = document;
+    }
+
+    public string getEmail()
+    {
+        return email;
+    }
+    public void setEmail()
+    {
+        this.email = email;
+    }
+
+    public string getPhone()
+    {
+        return phone;
+    }
+    public void setPhone()
+    {
+        this.phone = phone;
+    }
+
+    public string getLogin()
+    {
+        return login;
+    }
+    public void setLogin(string login)
+    {
+        this.login = login;
+    }
 }
