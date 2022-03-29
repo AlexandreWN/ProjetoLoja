c256dd57cb74fac2f395d00fa099db99c2a2afa6
Alexandre <alexandrenikitin@outlook.com>
2022-03-29 14:32:41 -0300

    adicionando dependencia WishList

--- Aulas_C/Model/WishList.cs
+++ Aulas_C/Model/WishList.cs
@@ -1,5 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
 namespace Model;
-public class WishList
+class WishList
 {
-    public void addProductToWishList(Product product) { }
+    private Product product;
+
+    public void addProductToWishList(Product product) {
+        List<Product> products = new List<Product>();
+    }
+
 }
