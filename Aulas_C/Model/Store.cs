3af2e13e9393830a239a4718c429b33020a55f8d
Alexandre <alexandrenikitin@outlook.com>
2022-03-29 14:44:47 -0300

    Adicionando dependencia de Person

--- Aulas_C/Model/Store.cs
+++ Aulas_C/Model/Store.cs
@@ -1,14 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
 namespace Model;
 public class Store
-
 {
     private Owner owner; // dependencia a Owner
     private string name;
     private string cnpj;
 
+    private List<Purchase> purchases = new List<Purchase>();
+
     public void addNewPurchase(Purchase purchase)
     {
-
+        purchases.Add(purchase);
     }
     
 }
