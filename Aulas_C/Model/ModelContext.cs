33a76cc44290d399b8efc96ae1e44c67f0ba4ef7
Alexandre <alexandrenikitin@outlook.com>
2022-04-05 16:50:29 -0300

    criando ModelContext

--- Aulas_C/Model/ModelContext.cs
+++ Aulas_C/Model/ModelContext.cs
@@ -1,11 +1,13 @@
 using Interfaces;
+using System;
+using Microsoft.EntityFrameworkCore;
 namespace Model;
 
-using Microsoft.EntityFrameworkCore;
 public class ModelContext : DbContext
 {
-	public ModelContext() : base("ProjetoLoja")
+	public ModelContext()
     {
+        //Mapeamento de entidade para tabela
         public DbSet<Address> Address { get; set; }
         public DbSet<Client> Client { get; set; }
         public DbSet<Owner> Owner { get; set; }
@@ -15,8 +17,17 @@ public class ModelContext : DbContext
         public DbSet<Stocks> Stocks { get; set; }
         public DbSet<Store> Store { get; set; }
         public DbSet<WishList> WishList { get; set; }
+        
+        //provedor e string de conexão
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Data Source=(local)\JVLPC0565\SQLEXPRESS;Database=ProjetoLojaTeste;Integrated Security=sspi;")
+        }
 
-    protected override void On
-}
-    public DbSet<>
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+            modelBuilder.Properties<string>().Where(p => p.Name == "document").Configure(p => p.IsKey());
+        }
+    }
 }
