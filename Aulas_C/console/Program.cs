f2d7a6f789fc3b908c2d361371bcd044bf5f0e67
Alexandre <alexandrenikitin@outlook.com>
2022-04-11 16:30:13 -0300

    teste

--- Aulas_C/console/Program.cs
+++ Aulas_C/console/Program.cs
@@ -1,2 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Text;
+using DAO;
+
+
+      using(var context = new LibraryContext())
+      {
+        // Creates the database if not exists
+        context.Database.EnsureCreated();
+
+
+        // Adds some books
+        context.Product.Add(new Product
+        {
+          id = 1,
+          name = "The Lord of the Rings",
+          unit_price = 1.1,
+          bar_code = "English",
+        });
+
+        // Saves changes
+        context.SaveChanges();
+      }
+      /*
+      // Gets and prints all books in database
+      using (var context = new LibraryContext())
+      {
+        var books = context.Product;
+        foreach(var book in books)
+        {
+          var data = new StringBuilder();
+          data.AppendLine($"ISBN: {book.id}");
+          data.AppendLine($"Title: {book.unit_price}");
+          data.AppendLine($"Publisher: {book.name}");
+          data.AppendLine($"Publishersss: {book.bar_code}");
+          Console.WriteLine(data.ToString());
+        }
+      }
+      */
