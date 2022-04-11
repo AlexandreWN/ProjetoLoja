d695dbabab179339512d0fb22079b6d46c2cfa33
Alexandre <alexandrenikitin@outlook.com>
2022-04-11 15:30:47 -0300

    modelBuilder

--- Aulas_C/DAO/LibraryContext.cs
+++ Aulas_C/DAO/LibraryContext.cs
@@ -11,7 +11,6 @@ public class LibraryContext : DbContext
         public DbSet<Address> Address { get; set; }
         public DbSet<Client> Client { get; set; }
         public DbSet<Owner> Owner { get; set; }
-        public DbSet<Person> Person { get; set; }
         public DbSet<Product> Product { get; set; }
         public DbSet<Purchase> Purchase { get; set; }
         public DbSet<Stocks> Stocks { get; set; }
@@ -25,29 +24,69 @@ public class LibraryContext : DbContext
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-<<<<<<< HEAD
+            modelBuilder.Entity<Address>(entity =>
+            {
+                entity.Haskey(a => a.Id);
+                entity.Property(a => a.street);
+                entity.Property(a => a.city);
+                entity.Property(a => a.state);
+                entity.Property(a => a.country);
+                entity.Property(a => a.post_code);
+            });
             modelBuilder.Entity<Client>(entity =>
             {
 
-                entity.HasKey(p => p.Id);
-                entity.Property(p => p.name).IsRequired();
-                entity.Property(p => p.date_of_birth).IsRequired();
-                entity.Property(p => p.document).IsRequired();
-                entity.Property(p => p.email).IsRequired();
-                entity.Property(p => p.phone).IsRequired();
-                entity.Property(p => p.login).IsRequired();
+                entity.HasKey(p => p.id);
+                entity.Property(p => p.name);
+                entity.Property(p => p.date_of_birth);
+                entity.Property(p => p.document);
+                entity.Property(p => p.email);
+                entity.Property(p => p.phone);
+                entity.Property(p => p.login);
             });
             modelBuilder.Entity<Owner>(entity =>
             {
-                entity.HasKey(p => p.Id);
-                entity.Property(p => p.name).IsRequired();
-                entity.Property(p => p.date_of_birth).IsRequired();
-                entity.Property(p => p.document).IsRequired();
-                entity.Property(p => p.email).IsRequired();
-                entity.Property(p => p.phone).IsRequired();
-                entity.Property(p => p.login).IsRequired();
+                entity.HasKey(p => p.id);
+                entity.Property(p => p.name);
+                entity.Property(p => p.date_of_birth);
+                entity.Property(p => p.document);
+                entity.Property(p => p.email);
+                entity.Property(p => p.phone);
+                entity.Property(p => p.login);
             });
-=======
->>>>>>> 6765947ef58cfb7277eeca4db0d225d34c1ee2dc
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Haskey(pr => pr.id);
+                entity.Property(pr => pr.name);
+                entity.Property(pr => pr.unit_price);
+                entity.Property(pr => pr.bar_code);
+            });
+            modelBuilder.Entity<Purchase>(entity =>
+            {
+                entity.Haskey(pu => pu.id);
+                entity.HasOne(pr => pr.Product).WithMany(pu => pu.Purchase);
+                entity.HasOne(p => p.Client).WithMany(pu => pu.Purchase);
+                entity.Property(pu => pu.payment);
+                entity.Property(pu => pu.number_confirmation);
+                entity.Property(pu => pu.number_nf);
+                entity.Property(pu => pu.payment_type);
+                entity.Property(pu => pu.puchaseStatusEnum);
+            });
+            modelBuilder.Entity<Stocks>(entity =>
+            {
+                entity.HasKey(s => s.id);
+                entity.Property(s => s.product);
+                entity.Property(s => s.quantity);
+            });
+            modelBuilder.Entity<Store>(entity =>
+            {
+                entity.HasKey(st => st.id);
+                entity.Property(st => st.name);
+                entity.Property(st => st.cnpj);
+            });
+            modelBuilder.Entity<WishList>(entity =>
+            {
+
+            })
         }
 }
