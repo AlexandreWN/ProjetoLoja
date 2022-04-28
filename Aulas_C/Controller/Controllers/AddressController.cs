3cd32fdb387d6f9e161966b128a42b276ae4e76d
Alexandre <alexandrenikitin@outlook.com>
2022-04-28 16:18:01 -0300

    ajustando

--- Aulas_C/Controller/Controllers/AddressController.cs
+++ Aulas_C/Controller/Controllers/AddressController.cs
@@ -6,14 +6,14 @@ using Microsoft.AspNetCore.Mvc;
 namespace Controller.Controllers;
 
 [ApiController]
-[Route("[controller]")]
-public class AddressController : ControllerBase{
-
+[Route("Address")]
+public class AddressController : ControllerBase {
     [HttpPost]
     [Route("register")]
     public object registerAddress([FromBody] AddressDTO address){
         return new {
-            response = "rota ok"
+            street = address.street,
+            state = address.state
         };
     }
 }
--- Aulas_C/Controller/Program.cs
+++ Aulas_C/Controller/Program.cs
@@ -6,6 +6,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers().AddNewtonsoftJson();
+
 
 var app = builder.Build();
 
