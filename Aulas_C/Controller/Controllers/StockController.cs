10991de6ae68130214366acc95b1d18b488a795b
Thiago <thiagopedron70@gmail.com>
2022-04-28 14:43:26 -0300

    Criando SotockController

--- Aulas_C/Controller/Controllers/StockController.cs
+++ Aulas_C/Controller/Controllers/StockController.cs
@@ -5,5 +5,6 @@ namespace Controller.Controllers;
 [ApiController]
 [Route("[controller]")]
 public class StockController : ControllerBase{
-    
+    public void addProductToStock(object request) { }
+    public void updateStock(object request) { }
 }
