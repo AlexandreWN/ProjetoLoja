366d53202d2e65bcf4df9b438df547d5a1a46527
Thiago <thiagopedron70@gmail.com>
2022-05-02 13:20:23 -0300

    Criando registerStore

--- Aulas_C/Controller/Controllers/StoreController.cs
+++ Aulas_C/Controller/Controllers/StoreController.cs
@@ -1,9 +1,20 @@
+using System;
+using Model;
+using DTO;
 using Microsoft.AspNetCore.Mvc;
-
 namespace Controller.Controllers;
 
 [ApiController]
 [Route("[controller]")]
 public class StoreController : ControllerBase{
-
+    [HttpPost]
+    [Route("register")]
+    public object registerStore([FromBody] StoreDTO store){
+              return new{
+                     id = store.id,
+                     name = store.name,
+                     CNPJ = store.CNPJ,
+                     owner  = store.owner,
+              };
+    }
 }
