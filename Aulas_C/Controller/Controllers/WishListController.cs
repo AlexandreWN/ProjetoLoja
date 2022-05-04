348809efcb14bf7822968fc6b5f1ea3f9751ebec
Alexandre <alexandrenikitin@outlook.com>
2022-05-04 13:55:37 -0300

    Alterando wishList

--- Aulas_C/Controller/Controllers/WishListController.cs
+++ Aulas_C/Controller/Controllers/WishListController.cs
@@ -7,12 +7,16 @@ namespace Controller.Controllers;
 [ApiController]
 [Route("[controller]")]
 public class WishListController : ControllerBase{
+    /*
     [HttpPost]
     [Route("register")]
     public object addProductToWishList([FromBody] WishListDTO wishList){
+        var wishListModel = Model.WishList.convertDTOToModel(wishList);
+        var id = wishListModel.save();
         return new{
             client = wishList.client,
             product = wishList.product
         };
     }
+    */
 }
