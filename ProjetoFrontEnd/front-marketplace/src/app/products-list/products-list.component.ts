e8d2d2f67993ba43f3676e5a8cd8be53580abbcf
ThiagoPedron <thiagopedron70@gmail.com>
2022-05-11 16:35:58 -0300

    Front até o erro do cors

--- ProjetoFrontEnd/front-marketplace/src/app/products-list/products-list.component.ts
+++ ProjetoFrontEnd/front-marketplace/src/app/products-list/products-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { products} from '../products';   
+import axios from "axios";
+
 
 @Component({
   selector: 'app-products-list',
@@ -12,23 +14,20 @@ export class ProductsListComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    var axios = require('axios');
-var data = '';
-
-var config = {
-  method: 'get',
-  url: 'http://localhost:5141/Product/getAll',
-  headers: { },
-  data : data
-};
-
-axios(config)
-.then(function (response) {
-  console.log(JSON.stringify(response.data));
-})
-.catch(function (error) {
-  console.log(error);
-});
+    var config = {
+      method: 'get',
+      url: 'https://localhost:7273/Product/getAll',
+      headers: { },
+      data : ''
+    };
+    
+    axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
     
   }
 
