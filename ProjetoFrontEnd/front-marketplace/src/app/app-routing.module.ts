097418576185d26c4679374f35349cd21a94662a
ThiagoPedron <thiagopedron70@gmail.com>
2022-06-02 14:51:56 -0300

    Finalizando Cadastro

--- ProjetoFrontEnd/front-marketplace/src/app/app-routing.module.ts
+++ ProjetoFrontEnd/front-marketplace/src/app/app-routing.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { WishlistDetailComponent } from './wishlist-detail/wishlist-detail.component';
-import { AddressRegisterComponent } from './address-register/address-register.component';
 import { ClientRegisterComponent } from './client-register/client-register.component';
 import { StoreRegisterComponent } from './store-register/store-register.component';
 
 const routes: Routes = [
   {path:'login',component:LoginComponent},
   {path:'wishlist-detail',component:WishlistDetailComponent},
-  {path:'address-register',component:AddressRegisterComponent},
   {path:'client-register',component:ClientRegisterComponent},
   {path:'store-register',component:StoreRegisterComponent}
 ];
