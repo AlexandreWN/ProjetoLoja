ceae6ea21e21b3b7482b1a3fdb4df174aa73c8a1
Alexandre <alexandrenikitin@outlook.com>
2022-06-02 14:10:42 -0300

    Register Client Com o Banco

--- ProjetoFrontEnd/front-marketplace/src/app/client-register/client-register.component.ts
+++ ProjetoFrontEnd/front-marketplace/src/app/client-register/client-register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import axios from "axios";
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-client-register',
   templateUrl: './client-register.component.html',
@@ -7,9 +8,54 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientRegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
   }
-
+  submit(){
+    let name  = document.getElementById("name") as HTMLInputElement;
+    let phone = document.getElementById("phone") as HTMLInputElement;
+    let email = document.getElementById("email") as HTMLInputElement;
+    let dateOfBirth = document.getElementById("date-of-birth") as HTMLInputElement;
+    let password = document.getElementById("password") as HTMLInputElement;
+    let documento = document.getElementById("password") as HTMLInputElement;
+    
+    let street = document.getElementById("street") as HTMLInputElement;
+    let state = document.getElementById("state") as HTMLInputElement;
+    let city = document.getElementById("city") as HTMLInputElement;
+    let country = document.getElementById("country") as HTMLInputElement;
+    let postal_code = document.getElementById("postal_code") as HTMLInputElement;
+    
+    var data = JSON.stringify({
+      "name": name.value,
+      "phone": phone.value,
+      "email": email.value,
+      "date_of_birth": dateOfBirth.value,
+      "document": documento.value,
+      "passwd": password.value,
+      "address": {
+        "street": street.value,
+        "state": state.value,
+        "city":city.value,
+        "country":country.value,
+        "postal_code":postal_code.value
+      }
+    });
+    
+    var config = {
+      
+      method: 'post',
+      url: 'http://localhost:5141/Client/register',
+      headers: { 
+        'Content-Type': 'application/json'
+      },
+      data : data
+    };
+    let instance = this;
+    axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(data));
+      instance.router.navigate(['/']);
+    })
+  }
 }
